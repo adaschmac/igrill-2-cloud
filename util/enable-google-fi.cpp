0c761109bccda9727fbc8aec1c9bf2cf4ad3e3d4
Adam Schmackpfeffer <adam.schmackpfeffer@ni.com>
2019-10-09 13:04:15 -0500

    Add code in loop() to determine if cell is active
    Still haven't gotten any blinkenlights though, weird...

--- util/enable-google-fi.cpp
+++ util/enable-google-fi.cpp
@@ -11,13 +11,14 @@
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
+static PinState ps = LOW;
+
 void setup() {
     Cellular.setActiveSim(EXTERNAL_SIM);
     Cellular.setCredentials("h2g2");
 
-    // This clears the setup done flag on brand new devices so it won't stay in listening mode
-    const uint8_t val = 0x01;
-    dct_write_app_data(&val, DCT_SETUP_DONE_OFFSET, 1);
+    Cellular.on();
+    Cellular.connect();
 
     // This is just so you know the operation is complete
     pinMode(D7, OUTPUT);
@@ -25,4 +26,9 @@ void setup() {
 }
 
 void loop() {
+    ps = ps == LOW ? HIGH : LOW;
+    pinMode(D7, OUTPUT);
+    digitalWrite(D7, ps);
+
+    delay(Cellular.ready() ? 1000 : 250);
 }
